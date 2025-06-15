a94fb44c91c675dee266ebfda7164fc7bdeeb863
sangramSA <sangramshinde.a@gmail.com>
2025-06-15 19:41:47 -0400

    Refactor test_connection.py to uncomment import statements and ensure the parent directory is added to the Python path

--- aura-db/scripts/test_connection.py
+++ aura-db/scripts/test_connection.py
@@ -1,8 +1,8 @@
-# import sys
-# import os
+import sys
+import os
 
-# # Add the parent directory to the Python path
-# sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+# Add the parent directory to the Python path
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 # from config.neo4j_config import Neo4jConfig
 
